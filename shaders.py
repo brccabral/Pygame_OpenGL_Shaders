d852f6c71b0352dbeb359c6b33833e95ca1c9391
Bruno Cabral <bruno.cabral@amdocs.com>
2023-03-21 16:14:10 -0700

    create and set shaders

--- shaders.py
+++ shaders.py
@@ -49,6 +49,38 @@ quad_buffer = ctx.buffer(
     )
 )
 
+vert_shader = """
+#version 330 core
+
+in vec2 vert; // position
+in vec2 texcoord; // uvs
+out vec2 uvs;
+
+void main() {
+    uvs = texcoord;
+    gl_Position = vec4(vert, 0.0, 1.0);
+}
+"""
+
+# uniform - passed to all parallel gpu
+# in - passed by each call a different buffer value
+frag_shader = """
+#version 330 core
+
+uniform sampler2D tex;
+
+in vec2 uvs;
+out vec4 f_color;
+
+void main() {
+    f_color = vec4(texture(tex, uvs).rgb, 1.0);
+}
+"""
+
+program = ctx.program(vertex_shader=vert_shader, fragment_shader=frag_shader)
+# (2f 2f) => (vert texcoord)
+render_object = ctx.vertex_array(program, [(quad_buffer, "2f 2f", "vert", "texcoord")])
+
 while True:
     screen.fill((0, 0, 0))
     screen.blit(img, pygame.mouse.get_pos())
